dc464e37f76eda21ddf3d1abd756532e68e91cbe
Nahom93-Mulualem <jurasmulualem@gmail.com>
2024-11-08 01:32:15 -0800

    master branch commited

---
+++ course-service/insert_mock_courses.sql
@@ -0,0 +1,13 @@
+USE course_service_db;
+
+INSERT INTO courses (name, description, teacher_id) VALUES
+('Introduction to Computer Science', 'Basic concepts of programming and computer science.', 2),
+('Data Structures', 'Introduction to data structures and algorithms.', 2),
+('Web Development', 'Learn to build websites using HTML, CSS, and JavaScript.', 5),
+('Database Management', 'Understanding relational databases and SQL.', 6),
+('Software Engineering', 'Principles of software engineering and project management.', 6),
+('Operating Systems', 'Introduction to operating systems concepts and design.', 2),
+('Machine Learning', 'Basics of machine learning algorithms and applications.', 5),
+('Cybersecurity', 'Principles of cybersecurity and risk management.', 6),
+('Mobile App Development', 'Develop applications for mobile devices.', 5),
+('Data Science', 'Data analysis and visualization techniques.', 2);
